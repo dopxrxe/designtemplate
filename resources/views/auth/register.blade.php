855dd0c944c05d773b798e980cbdd07237f7139f
dira okta prindana <diraoktaprindana.code@gmail.com>
2023-12-27 21:26:01 +0700

    login & register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,51 +2,41 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div class="vh-100 bg-gray-300">
+            <div class="row g-0">
+                <div class="col-8"></div>
+                <div class="col-4">
+                    <div class="p-3 vh-100">
+                        <div class="p-5 bg-white h-100 d-flex flex-column justify-content-center align-items-center gap-2 rounded-4">
+                            <img src="{{url('/assets/images/logo/Logo-PTDI-x-INHAN.png')}}" class="mb-4" width="96" height="96" alt="">
+                            <h1 class="fw-semibold">
+                                Welcome
+                            </h1>
+                            <p>Register with your e-mail</p>
+                            <div class="w-100 mt-4">
+                                <div class="mb-4">
+                                    <x-forms.elements.inputname id="name" class="" type="text" name="name" :label="__('Name')" :value="old('name')"/>
+                                    <x-input-error :messages="$errors->get('name')" class="" />
+                                </div>
+                                <div class="mb-4">
+                                    <x-forms.elements.inputemail type="email" id="email" name="email" class="" :label="__('Email')" :value="old('email')"/>
+                                    <x-input-error :messages="$errors->get('email')" class="" />
+                                </div>
+                                <div class="mb-4">
+                                    <x-forms.elements.inputpassword type="password" id="password" name="password" class="mb-4" :label="__('Password')" :value="old('password')"/>
+                                    <x-input-error :messages="$errors->get('password')" class="" />
+                                </div>
+                                <div class="mb-4">
+                                    <x-forms.elements.inputpassword type="password" id="password_confirmation" name="password_confirmation" class="mb-4" :label="__('Confirm Password')" :value="old('password_confirmation')"/>
+                                    <x-input-error :messages="$errors->get('password_confirmation')" class="" />
+                                </div>
+                                <x-primary-button class="w-100 rounded-pill">{{ __('Create Account') }}</x-primary-button>
+                            </div>
+                            <p class="fw-lighter mt-5">Already registered? <a class="fw-semibold text-dark text-decoration-none" href="{{ route('register') }}">{{ __('Log in now') }}</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </form>
 </x-guest-layout>
