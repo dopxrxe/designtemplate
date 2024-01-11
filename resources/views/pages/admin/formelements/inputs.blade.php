8be8491752d6d6e945d4d461d8ef07f5ae495789
dopxrxe <diraoktaprindana.code@gmail.com>
2024-01-11 16:03:45 +0700

    input text

--- resources/views/pages/admin/formelements/inputs.blade.php
+++ resources/views/pages/admin/formelements/inputs.blade.php
@@ -28,6 +28,8 @@
                                 <div class="tab-content" id="pills-tabContent1">
                                     <div class="tab-pane fade show active" id="default-input-preview" role="tabpanel" aria-labelledby="default-input-preview-tab">
                                         <div class="h-200 d-flex align-items-center">
+
+                                            {{-- START: Input Text --}}
                                             <div class="form-group w-100">
                                                 <h5 class="mb-1">Text Input <span class="required">*</span></h5>
                                                 <div class="controls mb-1">
@@ -45,6 +47,8 @@
                                                 </div>
                                                 <p>Max character number of <code>35 character</code> or less.</p>
                                             </div>
+                                            {{-- END: Input Text --}}
+                                            
                                         </div>
                                     </div>
                                     <div class="tab-pane fade" id="default-input-code" role="tabpanel" aria-labelledby="default-input-code-tab">
@@ -104,6 +108,7 @@
                                 <div class="tab-content" id="pills-tabContent2">
                                     <div class="tab-pane fade show active" id="input-telepon-preview" role="tabpanel" aria-labelledby="input-telepon-preview-tab">
                                         <div class="h-200 d-flex align-items-center">
+                                            {{-- START: Input Telpone Number --}}
                                             <div class="form-group w-100">
                                                 <h5 class="mb-1">Telephone Number</h5>
                                                 <div class="controls mb-1">
@@ -120,6 +125,64 @@
                                                 </div>
                                                 <p>Max telephone number of <code>12 digits</code> or less.</p>
                                             </div>
+                                            {{-- END: Input Telpone Number --}}
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane fade" id="input-telepon-code" role="tabpanel" aria-labelledby="input-telepon-code-tab">
+                                        <div class="code-box h-200">
+                                            <a class="btn-copy" data-clipboard-action="copy" data-clipboard-target="#inputtlp">
+                                                <i id="fa-copy" class="fa-regular fa-copy"></i>
+                                                <i id="fa-check" class="d-none fa-solid fa-check fa-lg text-success"></i>
+                                            </a>
+                                            <div id="inputtlp">
+                                                <p>1</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title mb-1">Telephone Number</h4>
+                                <p class="">Example of a basic input group with font awesome icons and validation</p>
+                            </div>
+
+                            <div class="card-body pt-0">
+                                <div class="nav-box wm-content mb-1">
+                                    <ul class="nav nav-pills d-flex justify-content-between" id="pills-tab-2" role="tablist">
+                                        <li class="nav-item">
+                                            <a class="nav-link p-25 active" id="input-telepon-preview-tab" data-toggle="pill" href="#input-telepon-preview" role="tab" aria-controls="input-telepon-preview" aria-selected="true"><p>Preview</p></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link p-25" id="input-telepon-code-tab" data-toggle="pill" href="#input-telepon-code" role="tab" aria-controls="input-telepon-code" aria-selected="false"><p>Code</p></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="tab-content" id="pills-tabContent2">
+                                    <div class="tab-pane fade show active" id="input-telepon-preview" role="tabpanel" aria-labelledby="input-telepon-preview-tab">
+                                        <div class="h-200 d-flex align-items-center">
+                                            {{-- START: Input Telpone Number --}}
+                                            <div class="form-group w-100">
+                                                <h5 class="mb-1">Email</h5>
+                                                <div class="controls mb-1">
+                                                    <fieldset>
+                                                        <div class="input-group">
+                                                            <div class="input-group-prepend">
+                                                                <span class="input-group-text" id="basic-addon1">
+                                                                    <i class="fa-solid fa-envelope"></i>
+                                                                </span>
+                                                            </div>
+                                                            <x-forms.elements.inputemail id="email" name="email" class="form-control" :label="__('text@example.com')" :value="old('email')"/>
+                                                        </div>
+                                                    </fieldset>
+                                                </div>
+                                            </div>
+                                            {{-- END: Input Telpone Number --}}
                                         </div>
                                     </div>
                                     <div class="tab-pane fade" id="input-telepon-code" role="tabpanel" aria-labelledby="input-telepon-code-tab">
